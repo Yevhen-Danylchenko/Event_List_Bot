b8ab32c1f1e35f0cec8bfbf409ce5298eef93c17
Yevhen <yevhen_danylchenko@ukr.net>
2026-02-20 21:28:11 +0200

    1-5

--- My_Bot.py
+++ My_Bot.py
@@ -6,6 +6,24 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HealthHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
+def run_http():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), HealthHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_http, daemon=True).start()
 
 load_dotenv()
 API_TOKEN = os.getenv("BOT_TOKEN")
